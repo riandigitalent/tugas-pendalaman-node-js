af3d6e401ed51162934d982c904fb295058a92ad
rian.digitalent <rian.digitalent@gmail.com>
2020-10-12 23:10:52 +0700

    debug Handle upload files & serve files

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express()
 const dbmember = InitDatabase()
 initTable(dbmember)
 
-app.set('views', __dirname + '/pages/')
+app.set('views', __dirname + '/pages')
 app.set('view engine', 'html')
 app.engine('html', hbs.__express)
 
@@ -31,9 +31,9 @@ app.use(morgan('combined'))
 app.use(bodyPraser.urlencoded())
 
 // ambil file dengan path static
-app.use('/assets', express.static(__dirname + '/assets/'))
+app.use('/assets', express.static(__dirname + '/assets'))
     // 1 lagi buat folder photo
-app.use('/files', express.static(__dirname + '/files/'))
+app.use('/files', express.static(__dirname + '/files'))
 
 app.get('/', (req, res, next) => {
     res.send({ success: true })
@@ -53,27 +53,27 @@ app.get('/member', async(req, res, next) => {
 
 //code buat handle  Get Method
 app.get('/add-member', (req, res, next) => {
-    res.send({ success: true })
+    res.send(req.query)
 })
 
 //code buat handle  post Method
 app.post('/add-member', (req, res, next) => {
     console.log('Request', req.body)
-        //res.send(req.body)
-
     console.log('File', req.files)
-        //ambil file name
+
+
+    //ambil file name
     const fileName = Date.now() + req.files.photo.name
 
     //tulis file
-    fs.writeFile(path.join(__dirname, '/files/', fileName), req.files.photo.data, err => {
+    fs.writeFile(path.join(__dirname, '/files/', fileName), req.files.photo.data, (err) => {
         if (err) {
             console.error(err)
             return
         }
 
         // insert member
-        insertmember(dbmember, req.body.name, parseInt(req.body.age), '/files/${fileName}')
+        insertmember(dbmember, req.body.name, parseInt(req.body.age), `/files/${fileName}`)
 
         // redirect refresh
         res.redirect('/member')
